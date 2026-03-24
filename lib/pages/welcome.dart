853b3352950ae73288ded870baf30292d1094a32
Meguireche Lamia <meguirechelamia@gmail.com>
2026-03-24 20:38:14 +0100

    welcome page done

--- lib/pages/welcome.dart
+++ lib/pages/welcome.dart
@@ -6,28 +6,86 @@ class Welcome extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          children: [
-            // top image
-            Image.asset('assets/images/Dante.jpg'),
-            // welcome text + another text beneath it
-            Text(
-              'Welcome!',
-              style: GoogleFonts.inter(
-                textStyle: TextStyle(
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage("assets/images/wallpaper.jpg"),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Center(
+          child: Column(
+            children: [
+              SizedBox(height: 165),
+              // welcome text + another text beneath it
+              Text(
+                'Welcome!',
+                style: GoogleFonts.poppins(
+                  textStyle: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey[800],
+                  ),
+                ),
+              ),
+              Text(
+                'Make your child more happy',
+                style: GoogleFonts.poppins(
+                  textStyle: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey[800],
+                  ),
+                ),
+              ),
+              SizedBox(height: 55,),
+              // another image
+              Image.asset('assets/images/smiley.jpg',height: 250,),
+
+              SizedBox(height: 75,),
+              // signup button
+
+              Opacity(
+                opacity: 0.70,
+                child: Container(
+                  alignment: Alignment(0, 0),
+                  width: 200,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    color: Colors.blue[100]
+                  ),
+                  child: Text('Sign Up',style: GoogleFonts.poppins(
+                    textStyle: TextStyle(fontSize: 20),
                   )),
-            ),
 
-            // another image
+                ),
+              ),
 
-            // signup button
+              SizedBox(height: 25,),
+              // login button
+              Opacity(
+                opacity: 0.70,
+                child: Container(
+                  alignment: Alignment(0, 0),
+                  width: 200,
+                  height: 50,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: Colors.pink[100]
+                  ),
+                  child: Text('Login',style: GoogleFonts.poppins(
+                    textStyle: TextStyle(fontSize: 20),
+                  )),
 
-            // login button
-          ],
+                ),
+              ),
+
+
+            ],
+          ),
         ),
       ),
     );
